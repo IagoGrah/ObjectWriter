d985e04696d96bbbcefe429e11372966734a4989
IagoGrah <iagobg02@gmail.com>
2021-07-12 00:02:30 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace ObjectWriter
+{
+    class Program
+    {
+        // Settings
+        static string wordsFilePath = $"words.txt";
+        static string fileNameFormat = "{0} - {1}";
+        static string dateFormat = "dd-MM-yy";
+        static string filePath = "Journal/";
+        static int timerSeconds = 600;
+
+        static void Main(string[] args)
+        {
+            LoadSettings();
+
+            var dateString = DateTime.Today.ToString(dateFormat);
+            var word = GenerateWord();
+            var fileName = String.Format(fileNameFormat, dateString, word);
+
+            System.IO.Directory.CreateDirectory(filePath);
+            var path = $"{filePath}{fileName}.txt";
+            
+            File.WriteAllText(path, $"{word}\n\n");
+            Process.Start("notepad.exe", path);
+
+            CountDown();
+
+            Console.WriteLine("Time's up!");
+            Thread.Sleep(3000);
+            Console.ReadKey();
+        }
+
+        static string GenerateWord()
+        {
+            string[] lines = File.ReadAllLines(wordsFilePath);
+
+            Random rand = new Random();
+            var word = lines[rand.Next(lines.Length)];
+            word = Char.ToUpper(word[0]) + word.Substring(1);
+            
+            return word;
+        }
+
+        static void CountDown()
+        {
+            for (int a = timerSeconds; a >= 0; a--)
+            {
+                var time = $"{(a/60).ToString("D2")}:{(a%60).ToString("D2")}";
+
+                Console.WriteLine("Write about the given word");
+                Console.Write("Time remaining: {0:D2}", time);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+
+        static void LoadSettings()
+        {
+            var lines = File.ReadAllLines("settings.txt").Where(x => x.Contains('=')).Select(x => x.Split('=')).ToDictionary((x => x[0]), (x => x[1].Trim('"')));
+            
+            wordsFilePath = lines["wordsFilePath"];
+            dateFormat = lines["dateFormat"];
+            fileNameFormat = lines["fileNameFormat"];
+            filePath = lines["filePath"];
+            timerSeconds = Int32.Parse(lines["timerSeconds"]);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# **Object Writer**
+
+This is a very small project I made to save myself some time while doing Object Writing.
+
+>_Object writing is a writing exercise that focuses on describing an object and any related experiences, using all of your senses. I originally learned about object writing from brilliant lyric guru Pat Pattison._
+
+All this program does is generate a word (from a given .txt file), and give you a timer.
+
+Upon running, using the ObjectWriter.bat file, it immediately opens up Notepad with the word as a title and starts the timer.
+
+Your files are saved in a folder, so you can look back at what you've written and see your own progress.
+
+You can also use the **settings.txt** file to change the timer, the file from which to pick words, the path where your files are saved, and how the file names are generated.
+
+I made this for myself but I'm glad to share. The file with all the words to pick from (**words.txt**) is not the best, but it gets the job done.
